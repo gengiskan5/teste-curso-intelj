7994f55584d31b89720370efe476cee51bad67c7
Alexandro Sousa <alexandrosousa01@gmail.com>
2023-03-15 20:45:45 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,23 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-
+//        String s = "o poblema dos 3 corpos, 300";
+//        Livro = new Livro(nome:"o poblema dos 3 corpos, 300");
        /* int a = 5;
         int b = 3;
         System.out.println("hello World! " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
